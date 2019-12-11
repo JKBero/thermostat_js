bbd6bf6724a358fef1a571424ba61786ffaf8106
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-11 15:54:07 +0000

    Add minimum temp constant and amend down function plus tests

--- spec/ThermoSpec.js
+++ spec/ThermoSpec.js
@@ -1,24 +1,31 @@
 'use strict';
 
-describe('Thermostat', function(){
+describe('Thermostat', function () {
 
     var thermostat;
 
-    beforeEach(function (){
-      thermostat = new Thermostat();
+    beforeEach(function () {
+        thermostat = new Thermostat();
     });
 
-    it ('has a starting temp of 20 degrees', function(){
-      expect(thermostat._temperature).toEqual(20);
+    it('has a starting temp of 20 degrees', function () {
+        expect(thermostat._temperature).toEqual(20);
     });
 
-    it ('increases the temp by 1 when using up function', function(){
-      thermostat.up();
-      expect(thermostat._temperature).toEqual(21);
+    it('increases the temp by 1 when using up function', function () {
+        thermostat.up();
+        expect(thermostat._temperature).toEqual(21);
     });
 
-    it ('decreases the temp by 1 when using down function', function(){
-      thermostat.down();
-      expect(thermostat._temperature).toEqual(19);
+    it('decreases the temp by 1 when using down function', function () {
+        thermostat.down();
+        expect(thermostat._temperature).toEqual(19);
+    });
+
+    it("doesn't go lower than 10 degrees", function () {
+        for (var i = 0; i < 11; i++) {
+            thermostat.down();
+        }
+        expect(thermostat._temperature).toEqual(10);
     });
 });
--- src/Thermo.js
+++ src/Thermo.js
@@ -2,12 +2,15 @@
 
 function Thermostat() {
     this._temperature = 20;
+    this._MINIMUM = 10;
 };
 
-Thermostat.prototype.up = function(){
-  this._temperature ++;
+Thermostat.prototype.up = function () {
+    this._temperature++;
 };
 
-Thermostat.prototype.down = function(){
-  this._temperature --;
+Thermostat.prototype.down = function () {
+    if (this._temperature !== this._MINIMUM) {
+        this._temperature--;
+    };
 };
