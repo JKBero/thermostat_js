5e0aff6fb65cc895f294b9f4bfe477830487a8cc
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-12-12 17:38:20 +0000

    Add weather viewing functionality and API

--- js/jquery.js
+++ js/jquery.js
@@ -4,21 +4,21 @@ $(document).ready(function() {
 
   var thermostat = new Thermostat();
 
-  $("#temp").text(`Current Temperature: ${thermostat.temperature}°`);
+  $("#temp").text(`Current Temperature: ${thermostat.temperature}°C`);
 
   $("#up").click(function() {
     thermostat.up();
-    $("#temp").text(`Current Temperature: ${thermostat.temperature}°`);
+    $("#temp").text(`Current Temperature: ${thermostat.temperature}°C`);
   });
 
   $("#down").click(function() {
     thermostat.down();
-    $("#temp").text(`Current Temperature: ${thermostat.temperature}°`);
+    $("#temp").text(`Current Temperature: ${thermostat.temperature}°C`);
   });
 
   $("#reset").click(function() {
     thermostat.reset();
-    $("#temp").text(`Current Temperature: ${thermostat.temperature}°`);
+    $("#temp").text(`Current Temperature: ${thermostat.temperature}°C`);
   });
 
   $("#energy-usage-button").click(function() {
@@ -40,6 +40,15 @@ $(document).ready(function() {
     thermostat.switchPsmOff();
     $(this).addClass('green');
     $("#psm-on").removeClass('green');
+  });
+
+  $("#city").click(function() {
+    var city = $("#city option:selected").text()
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=d3b4e49d7817c363daf01d1177040cef`, function(data) {
+      var temp = (data.main.temp - 273.15).toFixed(2);
+      $("#local-temp").text(`London weather: ${temp}°C`);
+      $("#local-temp").text(`${city} temperature: ${temp}`);
+    });
   });
 
 });
